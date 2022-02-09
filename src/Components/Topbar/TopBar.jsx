04389a5e7279cd292cc5e13c241cb157227f93e2
Shaad <ssidique610@gmail.com>
2022-02-09 13:23:26 +0530

    added material-ui/core , material-ui/icons and recharts

---
+++ src/Components/Topbar/TopBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./topbar.css";
+
+function TopBar() {
+  return (
+    <div className="topbar">
+      <div className="topbar-wrapper">
+        <div className="top-left">Left</div>
+        <div className="top-right">Right</div>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
